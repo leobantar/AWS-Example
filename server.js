89709ba54260c7701d40dde0fffe8495367f8fcd
leobantar <66851468+leobantar@users.noreply.github.com>
2021-06-01 07:53:11 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const server = http.createServer((req,res)=> {
-    res.end('<h1> Quien es el campeon de Fornite?</h1>')
+    res.end('<h1> Habrá que esperar un poco mas</h1>')
 } )
 
 server.listen(3000);
